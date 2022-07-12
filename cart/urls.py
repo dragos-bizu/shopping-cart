fd2562a9d949beb98f1c93250f4f4fcc57b6fede
xFreeDomFTW <36459451+xFreeDomFTW@users.noreply.github.com>
2022-07-12 19:40:10 +0300

    Create order model and endpoints

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cart.views import CartDetailListAPIView, CartAPIView
+from cart.views import CartDetailListAPIView, CartAPIView, CartCheckoutAPIView
 
 urlpatterns = [
     path('add/', CartAPIView.as_view(), name="cart_add"),
     path('details/', CartDetailListAPIView.as_view(), name="cart_details"),
+    path('checkout/', CartCheckoutAPIView.as_view()),
 ]
