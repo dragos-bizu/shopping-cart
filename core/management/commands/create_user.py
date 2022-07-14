bd23fa4adc4010f77a314851995b69538f788585
xFreeDomFTW <36459451+xFreeDomFTW@users.noreply.github.com>
2022-07-14 16:51:42 +0300

    Refactor create user command

--- core/management/commands/create_user.py
+++ core/management/commands/create_user.py
@@ -6,10 +6,10 @@ from core.models import UserProfile
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if User.objects.get(username='tim'):
+        if User.objects.filter(username='tim').exists():
             print('User already created')
         else:
             user = User.objects.create(username='tim', password='tim12345')
-            UserProfile.objects.create(user, wallet=1000, name='Tim',
+            UserProfile.objects.create(user=user, wallet=1000, name='Tim',
                                        address='Romania')
             print('User has been created!')
